556efe136772f96a18b41fd94e7008e099ed0f8a
stavros <samaras_st@yahoo.gr>
2025-09-08 02:51:35 +0300

    tempp

--- src/Components/DevelopersInfo.jsx
+++ src/Components/DevelopersInfo.jsx
@@ -1,7 +1,7 @@
 const DevelopersInfo = () => {
     return (
         <div id='developers' className="flex justify-center items-center bg-[#0a0a23] p-4 text-center text-sm md:text-base text-[#FFFFFF]">
-            <p>This page was designed and developed by Samaras Stavros - Junior Frontend Web Developer.!!!!!
+            <p>This page was designed and developed by Samaras Stavros - Junior Frontend Web Developer.
                 {/* <br />
                 Special thanks to
                 <strong><a target="_blank" href="https://www.uom.gr/"> #University-of-Macedonia </a></strong>
